73c377aa99be8a7db5c2ff8f916dbef793fac026
lenovo <596906@gmail.com>
2022-07-06 23:12:34 +0300

    in url.py add new path

--- confirm/urls.py
+++ confirm/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import start_page, show_one_guest, show_all_guests, FeedBackView, UpdateView, DoneView, FaqPage, GuestFeedBack, FeedBackViewUpdate
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('', start_page, name='main_page'),
@@ -11,5 +12,8 @@ urlpatterns = [
     path('rsvp/<int:id_feedback>', UpdateView.as_view()),
     path('list', GuestFeedBack.as_view()),
     path('done', DoneView.as_view()),
-    path('update/<int:pk>',FeedBackViewUpdate.as_view())
+    path('update/<int:pk>',FeedBackViewUpdate.as_view()),
+    path('favicon.ico/', RedirectView.as_view(url='/static/img/favicon/favicon.ico')),
 ]
+
+
